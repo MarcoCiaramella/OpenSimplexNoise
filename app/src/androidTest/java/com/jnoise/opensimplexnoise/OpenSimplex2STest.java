684fc4d9ad639b9af61f6a59cac247c589ef9419
MarcoCiaramella <marcociaramella87@gmail.com>
2021-06-15 15:51:52 +0200

    fixed CMakeLists.txt

---
+++ app/src/androidTest/java/com/jnoise/opensimplexnoise/OpenSimplex2STest.java
@@ -0,0 +1,37 @@
+package com.jnoise.opensimplexnoise;
+
+import android.content.Context;
+
+import androidx.test.ext.junit.runners.AndroidJUnit4;
+import androidx.test.platform.app.InstrumentationRegistry;
+
+import com.jnoise.opensimplexnoiselib.OpenSimplex2S;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+
+/**
+ * Instrumented test, which will execute on an Android device.
+ *
+ * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
+ */
+@RunWith(AndroidJUnit4.class)
+public class OpenSimplex2STest {
+
+    @Test
+    public void useAppContext() {
+        // Context of the app under test.
+        Context appContext = InstrumentationRegistry.getInstrumentation().getTargetContext();
+        assertEquals("com.jnoise.opensimplexnoise", appContext.getPackageName());
+    }
+
+    @Test
+    public void noise2_isCorrect(){
+        OpenSimplex2S openSimplex2S = new OpenSimplex2S();
+        double[] noise = openSimplex2S.noise2(1234, 512, 512);
+        assertNotNull(noise);
+    }
+}
