05dbbc7b445cdea668032e704642b22844b5128e
MarcoCiaramella <marcociaramella87@gmail.com>
2021-10-15 17:20:53 +0200

    added more noises in main activity

--- app/src/main/java/com/jnoise/opensimplexnoise/MainActivity.java
+++ app/src/main/java/com/jnoise/opensimplexnoise/MainActivity.java
@@ -9,26 +9,62 @@ import android.widget.ImageView;
 
 import com.jnoise.opensimplexnoiselib.OpenSimplex2F;
 
+import java.util.ArrayList;
+
 public class MainActivity extends AppCompatActivity {
+
+    private static final int WIDTH = 512;
+    private static final int HEIGHT = 512;
+    private static final int DELAY_MILLIS = 1000;
+    private final ArrayList<double[]> heightmaps = new ArrayList<>();
+    private final OpenSimplex2F openSimplex2F = new OpenSimplex2F(1234);
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        int width = 512;
-        int height = 512;
-        double[] noise = createHeightmap(width, height, 0, 0, 0.1);
-        int[] colors = new int[width*height];
+        heightmaps.add(createHeightmapNoise2(0, 0, 0.1));
+        heightmaps.add(createHeightmapNoise2(0, 0, 0.01));
+        heightmaps.add(createHeightmapNoise2XBeforeY(0, 0, 0.1));
+        heightmaps.add(createHeightmapNoise2XBeforeY(0, 0, 0.01));
+        heightmaps.add(createHeightmapNoise3Classic(0, 0, 0, 0.1));
+        heightmaps.add(createHeightmapNoise3Classic(0, 0, 0, 0.01));
+        heightmaps.add(createHeightmapNoise3XYBeforeZ(0, 0, 0, 0.1));
+        heightmaps.add(createHeightmapNoise3XYBeforeZ(0, 0, 0, 0.01));
+        heightmaps.add(createHeightmapNoise3XZBeforeY(0, 0, 0, 0.1));
+        heightmaps.add(createHeightmapNoise3XZBeforeY(0, 0, 0, 0.01));
+        heightmaps.add(createHeightmapNoise4Classic(0, 0, 0, 0, 0.1));
+        heightmaps.add(createHeightmapNoise4Classic(0, 0, 0, 0, 0.01));
+        heightmaps.add(createHeightmapNoise4XYBeforeZW(0, 0, 0, 0, 0.1));
+        heightmaps.add(createHeightmapNoise4XYBeforeZW(0, 0, 0, 0, 0.01));
+        heightmaps.add(createHeightmapNoise4XZBeforeYW(0, 0, 0, 0, 0.1));
+        heightmaps.add(createHeightmapNoise4XZBeforeYW(0, 0, 0, 0, 0.01));
+        heightmaps.add(createHeightmapNoise4XYZBeforeW(0, 0, 0, 0, 0.1));
+        heightmaps.add(createHeightmapNoise4XYZBeforeW(0, 0, 0, 0, 0.01));
+
+        ((ImageView)findViewById(R.id.imageView)).postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                if (heightmaps.size() > 0) {
+                    showImage(heightmaps.remove(0));
+                    ((ImageView)findViewById(R.id.imageView)).postDelayed(this, DELAY_MILLIS);
+                }
+            }
+        }, DELAY_MILLIS);
+    }
+
+    private void showImage(double[] noise){
+        int[] colors = new int[WIDTH*HEIGHT];
         for (int i = 0; i < noise.length; i++){
             double d = (noise[i]+1.0) / 2.0;
             colors[i] = Color.argb(0xFF, (int)(0xFF*d), (int)(0xFF*d), (int)(0xFF*d));
         }
-        Bitmap bitmap = Bitmap.createBitmap(colors, width, height, Bitmap.Config.ARGB_8888);
+        Bitmap bitmap = Bitmap.createBitmap(colors, WIDTH, HEIGHT, Bitmap.Config.ARGB_8888);
         ((ImageView)findViewById(R.id.imageView)).setImageBitmap(bitmap);
     }
 
-    private static double[] points(int width, int height, int offX, int offY, double freq){
+    private double[] points(int width, int height, int offX, int offY, double freq){
         double[] points = new double[width*height*2];
         int i = 0;
         for (int y = 0; y < height; y++) {
@@ -42,10 +78,88 @@ public class MainActivity extends AppCompatActivity {
         return points;
     }
 
-    public static double[] createHeightmap(int width, int height, int offX, int offY, double freq){
-        OpenSimplex2F openSimplex2F = new OpenSimplex2F(1234);
-        // create a width*height grid of points. Grid must be an array in the form of [x0,y0,x1,y1,....xn,yn]
-        double[] grid = points(width, height, offX, offY, freq);
+    private double[] points(int width, int height, int depth, int offX, int offY, int offZ, double freq){
+        double[] points = new double[width*height*depth*3];
+        int i = 0;
+        for (int z = 0; z < depth; z++) {
+            for (int y = 0; y < height; y++) {
+                for (int x = 0; x < width; x++) {
+                    double xd = (x + offX) * freq;
+                    double yd = (y + offY) * freq;
+                    double zd = (z + offZ) * freq;
+                    points[i++] = xd;
+                    points[i++] = yd;
+                    points[i++] = zd;
+                }
+            }
+        }
+        return points;
+    }
+
+    private double[] points(int width, int height, int depth, int time, int offX, int offY, int offZ, int offW, double freq){
+        double[] points = new double[width*height*depth*time*4];
+        int i = 0;
+        for (int w = 0; w < time; w++) {
+            for (int z = 0; z < depth; z++) {
+                for (int y = 0; y < height; y++) {
+                    for (int x = 0; x < width; x++) {
+                        double xd = (x + offX) * freq;
+                        double yd = (y + offY) * freq;
+                        double zd = (z + offZ) * freq;
+                        double wd = (w + offW) * freq;
+                        points[i++] = xd;
+                        points[i++] = yd;
+                        points[i++] = zd;
+                        points[i++] = wd;
+                    }
+                }
+            }
+        }
+        return points;
+    }
+
+    public double[] createHeightmapNoise2(int offX, int offY, double freq){
+        double[] grid = points(WIDTH, HEIGHT, offX, offY, freq);
         return openSimplex2F.noise2(grid, grid.length/2);
+    }
+
+    public double[] createHeightmapNoise2XBeforeY(int offX, int offY, double freq){
+        double[] grid = points(WIDTH, HEIGHT, offX, offY, freq);
+        return openSimplex2F.noise2XBeforeY(grid, grid.length/2);
+    }
+
+    public double[] createHeightmapNoise3Classic(int offX, int offY, int offZ, double freq){
+        double[] grid = points(WIDTH, HEIGHT, 1, offX, offY, offZ, freq);
+        return openSimplex2F.noise3Classic(grid, grid.length/3);
+    }
+
+    public double[] createHeightmapNoise3XYBeforeZ(int offX, int offY, int offZ, double freq){
+        double[] grid = points(WIDTH, HEIGHT, 1, offX, offY, offZ, freq);
+        return openSimplex2F.noise3XYBeforeZ(grid, grid.length/3);
+    }
+
+    public double[] createHeightmapNoise3XZBeforeY(int offX, int offY, int offZ, double freq){
+        double[] grid = points(WIDTH, HEIGHT, 1, offX, offY, offZ, freq);
+        return openSimplex2F.noise3XZBeforeY(grid, grid.length/3);
+    }
+
+    public double[] createHeightmapNoise4Classic(int offX, int offY, int offZ, int offW, double freq){
+        double[] grid = points(WIDTH, HEIGHT, 1, 1, offX, offY, offZ, offW, freq);
+        return openSimplex2F.noise4Classic(grid, grid.length/4);
+    }
+
+    public double[] createHeightmapNoise4XYBeforeZW(int offX, int offY, int offZ, int offW, double freq){
+        double[] grid = points(WIDTH, HEIGHT, 1, 1, offX, offY, offZ, offW, freq);
+        return openSimplex2F.noise4XYBeforeZW(grid, grid.length/4);
+    }
+
+    public double[] createHeightmapNoise4XZBeforeYW(int offX, int offY, int offZ, int offW, double freq){
+        double[] grid = points(WIDTH, HEIGHT, 1, 1, offX, offY, offZ, offW, freq);
+        return openSimplex2F.noise4XZBeforeYW(grid, grid.length/4);
+    }
+
+    public double[] createHeightmapNoise4XYZBeforeW(int offX, int offY, int offZ, int offW, double freq){
+        double[] grid = points(WIDTH, HEIGHT, 1, 1, offX, offY, offZ, offW, freq);
+        return openSimplex2F.noise4XYZBeforeW(grid, grid.length/4);
     }
 }
